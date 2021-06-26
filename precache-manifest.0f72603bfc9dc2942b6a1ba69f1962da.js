844e80b9727b6ace60352edb454aad53182060e3
mani-mani-mani <>
2021-06-26 18:20:16 +0000

    chore: update github-page

---
+++ precache-manifest.0f72603bfc9dc2942b6a1ba69f1962da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e893667a60bb09c2cfba524032e20624",
+    "url": "/colors/index.html"
+  },
+  {
+    "revision": "ee193551de575060826b",
+    "url": "/colors/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ac53702e60ba327e8a5",
+    "url": "/colors/static/js/2.c2cf87cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colors/static/js/2.c2cf87cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee193551de575060826b",
+    "url": "/colors/static/js/main.6710f748.chunk.js"
+  },
+  {
+    "revision": "7231f36ebb02d96eea82",
+    "url": "/colors/static/js/runtime-main.6eeb06f5.js"
+  }
+]);
